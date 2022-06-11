60035d56791ac40166546981626f7d1e0374da25
Lyndon Panton <lyndonpanton@gmail.com>
2022-06-11 02:02:22 +0100

    Set basic application template

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace FizzBuzzCSharp
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+
+		public List<int> FizzBuzz(int upperLimit)
+		{
+
+		}
+	}
+}
