31592c2510cced733e8a4d10ef7008dd379bcc8a
Lyndon Panton <lyndonpanton@gmail.com>
2022-06-11 02:09:55 +0100

    Created FizzBuzz logic

--- Program.cs
+++ Program.cs
@@ -13,7 +13,18 @@ namespace FizzBuzzCSharp
 
 		public void FizzBuzz(int upperLimit)
 		{
-			
+
+			for (int i = 1 i <= 20; i++)
+			{
+				if (i % 15 == 0)
+					Console.WriteLine("FizzBuzz");
+				else if (i % 5 == 0)
+					Console.WriteLine("Buzz");
+				else if (i % 3 == 0)
+					Console.WriteLine("Fizz");
+				else
+					Console.WriteLine(i);
+			}
 		}
 	}
 }
